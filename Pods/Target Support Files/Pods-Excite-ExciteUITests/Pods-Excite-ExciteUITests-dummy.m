aa182c320b3195920d0a0737ec4851e710b026a1
Hsanka6 <haasith2@gmail.com>
2020-05-22 16:53:04 -0700

    Removed pods

--- Pods/Target Support Files/Pods-Excite-ExciteUITests/Pods-Excite-ExciteUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Excite_ExciteUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Excite_ExciteUITests
-@end
--- Pods/Target Support Files/Pods-Excite/Pods-Excite-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Excite : NSObject
-@end
-@implementation PodsDummy_Pods_Excite
-@end
