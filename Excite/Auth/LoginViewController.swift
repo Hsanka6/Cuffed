668250eb1c2159bdf4c2d52e229335881d3d8a05
Jan Ephraim Nino Tanja <jantanja@Jans-MacBook-Pro.local>
2020-07-15 00:45:51 -0700

    Bruh lmao

--- Excite/Auth/LoginViewController.swift
+++ Excite/Auth/LoginViewController.swift
@@ -24,11 +24,47 @@ class Colors {
 
 class LoginViewController: UIViewController {
 
-    var loginButton = UIButton()
-    var logo = UITextView()
-    var stackView = UIStackView()
     let colors = Colors()
+    let mainView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.6, alpha: 0.4)
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    let logo: UILabel = {
+        let view = UILabel()
+        view.text = "Pinned"
+        view.textColor = .black
+        return view
+    }()
     
+    let usernameView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.red
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    let passwordView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.blue
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    let loginButton: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.green
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    lazy var loginStack: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [usernameView, passwordView, loginButton])
+        stackView.axis = .vertical
+        stackView.distribution = .equalCentering
+        return stackView
+    }()
     func constructBackground() {
         let backgroundLayer = colors.gl
         backgroundLayer.frame = view.frame
@@ -36,7 +72,8 @@ class LoginViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        constructBackground()
+        // constructBackground()
+        view.backgroundColor = .white
         makeUI()
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -45,58 +82,34 @@ class LoginViewController: UIViewController {
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
         // Show the navigation bar on other view controllers
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     func makeUI() {
-        let width = UIScreen.main.bounds.width - 20
-        self.view.addSubview(stackView)
-        // add the two subviews
-        stackView.addSubview(logo)
-        stackView.addSubview(loginButton)
+        view.addSubview(mainView)
         
-        stackView.spacing = 10.0
-        stackView.axis = .vertical
-        stackView.alignment = .center
-        stackView.distribution = .equalSpacing
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-
-        
-        stackView.snp.makeConstraints { (make) in
-            make.height.equalTo(80)
-            make.width.equalTo(width)
-            make.center.equalTo(self.view)
-        }
-        // modify the views here
-        
-
-        logo.textColor = .white
-        logo.backgroundColor = .black
-        logo.text = "ExciteDate"
-        logo.snp.makeConstraints { (make) -> Void in
+        mainView.addSubview(loginStack)
+        mainView.addSubview(logo)
+        usernameView.snp.makeConstraints { (make) in
             make.height.equalTo(50)
-            make.width.equalTo(200)
-            make.center.equalTo(self.view)
         }
-        
-        loginButton.setTitle("Login Screen", for: .normal)
-        loginButton.snp.makeConstraints { (make) -> Void in
-            make.height.equalTo(40)
-            make.width.equalTo(100)
-           make.center.equalTo(self.view)
+        passwordView.snp.makeConstraints { (make) in
+            make.height.equalTo(50)
         }
-//
-//        stackView.addSubview(logo)
-//        stackView.addSubview(loginButton)
-//
-//
-        
-        loginButton.addTarget(self, action: #selector(self.login), for: .touchUpInside)
+        mainView.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.top.equalTo(100)
+            make.width.equalTo(300)
+            make.bottom.equalTo(-20)
+        }
+        loginStack.snp.makeConstraints { (make) in
+            make.width.height.equalTo(300)
+            make.center.equalTo(self.mainView)
+        }
     }
-    @objc func login(sender: UIButton!) {
-       let newViewController = MainTabBarController()
-       self.navigationController?.pushViewController(newViewController, animated: true)
-    }
+//    @objc func login(sender: UIButton!) {
+//       let newViewController = MainTabBarController()
+//       self.navigationController?.pushViewController(newViewController, animated: true)
+//    }
 
 }
