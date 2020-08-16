1b01f11466169e3f9ddfb18323364c2c7cab60c1
Hsanka6 <haasith2@gmail.com>
2020-08-16 01:36:31 -0700

    Added most of models

--- Excite/Networking/Models.swift
+++ Excite/Networking/Models.swift
@@ -6,111 +6,245 @@
 //  Copyright © 2020 Haasith Sanka. All rights reserved.
 //
 
+import Foundation
+import UIKit
 
-//import Foundation
-//import UIKit
-//
-//struct User: Codable {
-//    let userId: String
-//    let profile: Profile
-//    let matches: [Date]
-//    let liked: [String] //your likes
-//    let likedYou: String //array of userIds that liked you
-//    let seen: [String] //array of seen users
-//    let blocked: [String] //array of blocked users
-//    let isPremium: Bool
+struct User: Codable {
+    let userId: String
+    let profile: Profile
+    let matches: [Date]
+    let liked: [String] //your likes
+    let likedYou: String //array of userIds that liked you
+    let seen: [String] //array of seen users
+    let blocked: [String] //array of blocked users
+    let isPremium: Bool
+}
+
+
+enum GenderType: String, Codable {
+    case MALE
+    case FEMALE
+    case OTHER
+}
+
+//class tenQuestions: Game {
+//    let question:[String] = []
 //}
-//
-//
-//enum GenderType: String, Codable {
-//    case MALE
-//    case FEMALE
-//    case OTHER
-//}
-//
-//struct tenQuestions: Game {
-//   let question:[String]
-//}
-//
-//struct Game: Codable {
-//   let id: String
-//}
-//
-//
-//
-//struct Date: Codable {
-//    let id: String
-//    let chatId: String
-//    let user1Id: String
-//    let user2Id: String
-//    let games: [String]
-//}
-//
-//struct Match: Date {
-//   let isMatch: bool
-//}
-//
-//
-//struct Profile: Codable {
-//    let photos: [String]
-//    let socials: [SocialProfile]
-//    let freeResponse: [FreeResponse]
-//    let lat: Double
-//    let lon: Double
-//    let personalDetails: PersonalDetails
-//    let answers: [MultipleChoiceAnswer]
-//    let personalityAnswers:  [Personality]
-//}
-//
-//Struct Personality: MultipleChoiceAnswer {
-//    let topValue: String //extroverted
-//    let bottomValue: String //introverted
-//}
-//
-//// “Your most embarrassing memory?”
-//// Open Ended questions and answers
-//struct FreeResponse: Codable {
-//    let question: String
-//    let answer: String
-//    let image: String
-//}
-//
-//struct MultipleChoiceAnswer: MultipleChoice, Codable {
-//    let answer: String
-//}
-//
-////  Question with limited answers
-//struct MultipleChoice: Codable {
-//    let id: String //maybe
-//    let question: String
-//    let answerChoices: [String]
-//}
-//
-//
-//struct PersonalDetails: Codable {
-//    let fullName: String
-//    let age: Int
-//    let height: String
-//    let gender: GenderType
-//    let ethnicity: String
-//    let location: String
-//    let jobTitle: String
-//    let company: String
-//}
-//
-//
-//struct SocialProfile: Codable {
-//    let platform: String
-//    let link: String
-//}
-//
-//struct Filter: Codable {
-//    let interest: [GenderType]
-//    let distance: Int
-//    let time: Availability
-//    let age: Int
-//    Let moreFilters: [MultipleChoice]
-//}
+
+class Game: Codable {
+   let id: String
+}
+
+
+
+class Dates: Codable {
+    let id: String
+    let chatId: String
+    let user1Id: String
+    let user2Id: String
+    let games: [String]
+    
+    init(id: String, chatId: String, user1Id: String, user2Id: String, games: [String]) {
+        self.id = id
+        self.chatId = chatId
+        self.user1Id = user1Id
+        self.user2Id = user2Id
+        self.games = games
+    }
+}
+
+class Match: Dates {
+   let isMatch: Bool
+    
+    init(isMatch: Bool, id: String, chatId: String, user1Id: String, user2Id: String, games: [String]) {
+           self.isMatch = isMatch
+       super.init(id: id, chatId: chatId, user1Id: user1Id, user2Id: user2Id, games: games)
+   }
+       
+       private enum Key: CodingKey {
+           case isMatch
+           case id
+           case chatId
+           case user1Id
+           case user2Id
+           case games
+       }
+       
+       required init(from decoder: Decoder) throws {
+           let container = try decoder.container(keyedBy: Key.self)
+           let isMatch = try container.decode(Bool.self, forKey: .isMatch)
+           let id = try container.decode(String.self, forKey: .id)
+           let chatId = try container.decode(String.self, forKey: .chatId)
+            let user1Id = try container.decode(String.self, forKey: .user1Id)
+            let user2Id = try container.decode(String.self, forKey: .user2Id)
+            let games = try container.decode(Array<String>.self, forKey: .games)
+            self.isMatch = isMatch
+           super.init(id: id, chatId: chatId, user1Id: user1Id, user2Id: user2Id, games: games)
+       }
+    
+}
+
+
+class Profile: Codable {
+    let photos: [String]
+    let socials: [SocialProfile]
+    let freeResponse: [FreeResponse]
+    let lat: Double
+    let lon: Double
+    let personalDetails: PersonalDetails
+    let answers: [MultipleChoiceAnswer]
+    let personalityAnswers:  [Personality]
+    
+    private enum CodingKeys: String, CodingKey {
+        case photos
+        case socials
+        case freeResponse
+        case lat
+        case lon
+        case personalDetails
+        case answers
+        case personalityAnswers
+    }
+    
+    init(photos: [String], socials: [SocialProfile], freeResponse: [FreeResponse], lat: Double, lon: Double, personalDetails: PersonalDetails, answers: [MultipleChoiceAnswer], personalityAnswers: [Personality]) {
+        self.photos = photos
+        self.socials = socials
+        self.freeResponse = freeResponse
+        self.lat = lat
+        self.lon = lon
+        self.personalDetails = personalDetails
+        self.answers = answers
+        self.personalityAnswers = personalityAnswers
+    }
+
+    required init(from decoder: Decoder) throws {
+        // Get our container for this subclass' coding keys
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let photos = try container.decode(Array<String>.self, forKey: .photos)
+        let socials = try container.decode(Array<SocialProfile>.self, forKey: .socials )
+        let freeResponse = try container.decode(Array<FreeResponse>.self, forKey: .freeResponse )
+        let lat = try container.decode(Double.self, forKey: .lat)
+        let lon = try container.decode(Double.self, forKey: .lon )
+        let personalDetails = try container.decode(PersonalDetails.self, forKey: .personalDetails )
+        let answers = try container.decode(Array<MultipleChoiceAnswer>.self, forKey: .answers )
+        let personalityAnswers = try container.decode(Array<Personality>.self, forKey: .personalityAnswers )
+        
+        self.photos = photos
+        self.socials = socials
+        self.freeResponse = freeResponse
+        self.lat = lat
+        self.lon = lon
+        self.personalDetails = personalDetails
+        self.answers = answers
+        self.personalityAnswers = personalityAnswers
+              
+        //super.init(num: num, food: food)
+        //fatalError("init(from:) has not been implemented")
+    }
+    
+    
+}
+
+class Personality: MultipleChoiceAnswer {
+    let topValue: String //extroverted
+    let bottomValue: String //introverted
+    
+    private enum Key: CodingKey {
+        case question
+        case answerChoices
+        case answer
+        case top
+        case bottom
+     }
+        
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Key.self)
+        let question = try container.decode(String.self, forKey: .question)
+        let answerChoices = try container.decode(Array<String>.self, forKey: .answerChoices)
+        let answer = try container.decode(String.self, forKey: .answer)
+        let topValue = try container.decode(String.self, forKey: .top)
+        let bottomValue = try container.decode(String.self, forKey: .bottom)
+        self.topValue = topValue
+        self.bottomValue = bottomValue
+        super.init(answer: answer, question: question, answerChoices: answerChoices)
+    }
+}
+
+// “Your most embarrassing memory?”
+// Open Ended questions and answers
+struct FreeResponse: Codable {
+    let question: String
+    let answer: String
+    let image: String
+}
+
+class MultipleChoiceAnswer: MultipleChoice {
+    let answer: String
+    
+    private enum Key: CodingKey {
+       case question
+       case answerChoices
+       case answer
+    }
+    init(answer: String, question: String, answerChoices:[String]) {
+        self.answer = answer
+        super.init(question: question, answerChoices: answerChoices)
+        
+    }
+       
+    required init(from decoder: Decoder) throws {
+       let container = try decoder.container(keyedBy: Key.self)
+       let question = try container.decode(String.self, forKey: .question)
+       let answerChoices = try container.decode(Array<String>.self, forKey: .answerChoices)
+       let answer = try container.decode(String.self, forKey: .answer)
+       self.answer = answer
+     super.init(question: question, answerChoices: answerChoices)
+   }
+}
+
+//  Question with limited answers
+class MultipleChoice: Codable {
+    //let id: String //maybe
+    let question: String
+    let answerChoices: [String]
+    
+    init(question:String, answerChoices: [String]) {
+        self.question = question
+        self.answerChoices = answerChoices
+    }
+}
+
+
+struct PersonalDetails: Codable {
+    let fullName: String
+    let age: Int
+    let height: String
+    let gender: GenderType
+    let ethnicity: String
+    let location: String
+    let jobTitle: String
+    let company: String
+}
+
+
+class SocialProfile: Codable {
+    let platform: String
+    let link: String
+    
+    init(platform: String, link: String) {
+        self.platform = platform
+        self.link = link
+    }
+}
+
+class Filter: Codable {
+    let interest: [GenderType]
+    let distance: Int
+    let age: Int
+    let moreFilters: [MultipleChoice]
+    
+    
+}
 
 
 //struct
@@ -134,7 +268,7 @@ class Food: Edible {
         super.init(name: name, size: size)
     }
     
-    enum Key: CodingKey {
+    private enum Key: CodingKey {
         case name
         case size
         case taste
@@ -174,3 +308,5 @@ class Test: Codable {
     }
       
 }
+
+
